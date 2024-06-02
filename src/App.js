c5c783b1c7f304810e0d62734add183069b75f0d
Amariah <amariah.abish@gmail.com>
2024-06-02 14:19:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// frontend/src/App.js
+
+import React from 'react';
+import './App.css';
+import Chatbot from './components/Chatbot';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Chatbot />
+            </header>
+        </div>
+    );
+}
+
+export default App;
